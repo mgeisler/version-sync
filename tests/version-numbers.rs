a44f3152d59b84d3f0e4f67a8168bbe1454ba6d9
Martin Geisler <martin@geisler.net>
2019-03-27 07:35:44 +0100

    readme: show minimum supported rustc version
    Also check that the README contains the same version number as the
    Travis config.

--- tests/version-numbers.rs
+++ tests/version-numbers.rs
@@ -12,6 +12,13 @@ fn test_readme_changelog() {
 }
 
 #[test]
+fn test_minimum_rustc_version() {
+    let version = "1.31.0";
+    version_sync::assert_contains_regex!(".travis.yml", &format!(r"^  - {}", version));
+    version_sync::assert_contains_regex!("README.md", &format!("badge/rustc-{}", version));
+}
+
+#[test]
 fn test_html_root_url() {
     version_sync::assert_html_root_url_updated!("src/lib.rs");
 }
